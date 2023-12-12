14a3c292fdfd648f38a990db2329979d8e006bf4
MATHEUSNUNES2023BR <143006952+MATHEUSNUNES2023BR@users.noreply.github.com>
2023-12-12 11:59:09 -0300

    Adicionado Login e Senha
    Foi adicionado uma requisição inicial de login e senha para ter acesso ao sistema

--- Cartorio.c
+++ Cartorio.c
@@ -110,56 +110,74 @@ int main()
 {
 	
 	setlocale(LC_ALL, "Portuguese");//Definindo a linguagem
+	printf("### CARTÓRIO EBAC ###\n");//inicio do menu
 	
 	int opcao = 0; //Definindo variáveis
 	int laco = 1;
 	char a;
-	for(laco=1; laco=1;){
-		
-		system("cls");
-		
-		printf("### CARTÓRIO EBAC ###\n");//inicio do menu
-		printf("Escolha a opção desejada no menu:\n");
-		printf("\t1 - Registrar nomes\n");
-		printf("\t2 - Consultar nomes\n");
-		printf("\t3 - Deletar nomes\n");
-		printf("\t4 - Sair\n\n");
-		printf("Opção: ");//fim do menu
-		
-		scanf("%d", &opcao);//armazenando a escolha do usuário
-		
-		system("cls");
-		switch(opcao){
-			case 1:
-				registro();
-				while(1)//Loop para continuar cadastrando usuário caso não aperte S ou s
-				{
-					printf("\nDeseja continuar? S/N ");
-					scanf("%s", &a);
-					if (a == 'S' || a == 's') {
-				    	printf("\nVocê optou por continuar\n");
-				    	registro();
-				  	} 
-					else {
-				    	printf("\nVocê optou por não continuar\n");
-				    	break;
-				  	}
-			  	}
-				break;
-			case 2:
-				consulta();
-				break;
-			case 3:
-				deletar();
-				break;
-			case 4:
-				return 0;
-				break;
-			default:
-				printf("Essa opção não está disponível!\n");
-				system("pause");
-				break;
-		}//fim da seleção
-	}	
 	
+	int complogin;
+	int compsenha;	
+	char login[10];
+	char senha [10];
+	
+	printf("Login: ");
+	scanf("%s", login);
+	printf("\nSenha: ");
+	scanf("%s", senha);
+	
+	complogin = strcmp(login, "ebac");
+	compsenha = strcmp(senha, "admin");
+	
+	if(complogin == 0 && compsenha == 0)
+	{
+
+		for(laco=1; laco=1;){
+			system("cls");
+			printf("### CARTÓRIO EBAC ###\n");//inicio do menu
+			printf("Escolha a opção desejada no menu:\n\n");
+			printf("\t1 - Registrar nomes\n");
+			printf("\t2 - Consultar nomes\n");
+			printf("\t3 - Deletar nomes\n");
+			printf("\t4 - Sair\n\n");
+			printf("Opção: ");//fim do menu
+			
+			scanf("%d", &opcao);//armazenando a escolha do usuário
+			
+			system("cls");
+			switch(opcao){
+				case 1:
+					registro();
+					while(1)//Loop para continuar cadastrando usuário caso não aperte S ou s
+					{
+						printf("\nDeseja continuar? S/N ");
+						scanf("%s", &a);
+						if (a == 'S' || a == 's') {
+					    	printf("\nVocê optou por continuar\n");
+					    	registro();
+					  	} 
+						else {
+					    	printf("\nVocê optou por não continuar\n");
+					    	break;
+					  	}
+				  	}
+					break;
+				case 2:
+					consulta();
+					break;
+				case 3:
+					deletar();
+					break;
+				case 4:
+					return 0;
+					break;
+				default:
+					printf("Essa opção não está disponível!\n");
+					system("pause");
+					break;
+			}//fim da seleção
+		}	
+	}else{
+		printf("\nLogin ou senha incorretos!");
+	}
 }
